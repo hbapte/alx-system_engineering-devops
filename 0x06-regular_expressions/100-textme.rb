5b184dafabe6bc122bdcf344e57b96fd09db5e51
hbapte <ijbapte@gmail.com>
2022-10-20 15:37:25 -0700

    regular expression in ruby

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
+#puts ARGV[0].scan(/\[([^S|A|B|F||u\]]+?)\]/).join(", ").scan(/<([^>]*)>/).last.first
+str = ARGV[0].scan(/\[([^\]([TFSu])]*)\]/).join(", ")
+str.split(/\w+:(.*), to:(.*), flags:(.*)/)
+puts str
