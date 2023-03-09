b2079d58129b5e616e643622d45f27dac48a6e34
hbapte <ijbapte@gmail.com>
2023-03-09 03:07:48 +0200

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    # Check if the user provided the employee ID as argument
+    if len(sys.argv) != 2:
+        print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))
+        sys.exit(1)
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    # Save employee ID provided by the user
+    employee_id = sys.argv[1]
+
+    # Get employee info
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    response = requests.get(user_url)
+    user_data = response.json()
+
+    # Get employee's TODO list
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    response = requests.get(todo_url)
+    todo_data = response.json()
+
+    # Extract completed tasks and count them
+    completed_tasks = [task for task in todo_data if task['completed']]
+    num_completed_tasks = len(completed_tasks)
+    num_total_tasks = len(todo_data)
+
+    # Print employee TODO list progress
+    print("Employee {} is done with tasks({}/{}):".format(user_data['name'], num_completed_tasks, num_total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task['title']))
